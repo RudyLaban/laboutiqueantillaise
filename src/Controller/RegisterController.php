30c1396b7fe5b35bba75d2c561dd627ca891952d
Rudy <lay971@hotmail.fr>
2021-05-30 21:03:24 +0200

    Enregistrement des membres en base via le formulaire

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,23 +4,51 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * RegisterController constructor.
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em =$em;
+    }
+
     /**
      * @Route("/inscription", name="register")
+     * @param Request $request
+     * @return Response
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $user = new User();
-        $registerForm = $this->createForm(RegisterType::class, $user);
+        $form = $this->createForm(RegisterType::class, $user);
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $user = $form->getData();
+
+            $this->em->persist($user);
+            $this->em->flush();
+        }
 
         return $this->render('register/index.html.twig', [
-            'registerForm' => $registerForm->createView(),
+            'registerForm' => $form->createView(),
         ]);
     }
 }
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,34 +19,24 @@ class RegisterType extends AbstractType
         $builder
             ->add('firstname', TextType::class, [
                 'label' => 'Prénom',
-                'attr' => [
-                    'placeholder' => 'Veuillez saisir votre prénom',
-                ],
+                'attr'  => ['placeholder' => 'Veuillez saisir votre prénom',],
             ])
             ->add('lastname', TextType::class, [
                 'label' => 'Nom',
-                'attr' => [
-                    'placeholder' => 'Veuillez saisir votre nom',
-                ],
+                'attr'  => ['placeholder' => 'Veuillez saisir votre nom'],
             ])
             ->add('email', EmailType::class, [
                 'label' => 'Email',
-                'attr' => [
-                    'placeholder' => 'Veuillez saisir votre email',
-                ],
+                'attr'  => ['placeholder' => 'Veuillez saisir votre email'],
             ])
-            ->add('password', PasswordType::class, [
-                'label' => 'Mot de passe',
-                'attr' => [
-                    'placeholder' => 'Veuillez saisir un mot de passe'
-                ],
-            ])
-            ->add('password_confirm', PasswordType::class, [
-                'label' => 'Mot de passe',
-                'mapped' => false,
-                'attr' => [
-                    'placeholder' => 'Veuillez confirmer le mot de passe'
-                ],
+            ->add('password', RepeatedType::class, [
+                'type'              => PasswordType::class,
+                'invalid_message'   => 'Le mot de passe et la confirmation doivent être identique',
+                'label'             => 'Mot de passe',
+                'required'          => true,
+                'first_options'     => ['label' => 'Mot de passe'],
+                'second_options'    => ['label' => 'Confirmation du mot de passe'],
+                'attr'              => ['placeholder' => 'Veuillez saisir un mot de passe'],
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'S\'inscrire'
