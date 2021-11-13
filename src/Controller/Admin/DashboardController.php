0d9808005c510cb8c41a3775d7222bd16b94d74c
Rudy <lay971@hotmail.fr>
2021-11-13 16:36:48 +0100

    Entités liées au tunnel d'achat
    * Carrier : Transporteur
    * Order : Commande 
    * OrderDetails : Détailles de commande

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Carrier;
 use App\Entity\Category;
 use App\Entity\Product;
 use App\Entity\User;
@@ -29,9 +30,10 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('Dashboard', 'fas fa-home');
+        yield MenuItem::linktoDashboard('Tableau de bord', 'fas fa-home');
         yield MenuItem::linkToCrud('Utilisateur', 'fas fa-user', User::class);
         yield MenuItem::linkToCrud('Categories', 'fas fa-tags', Category::class);
         yield MenuItem::linkToCrud('Produits', 'fas fa-barcode', Product::class);
+        yield MenuItem::linkToCrud('Transporteurs', 'fas fa-truck', Carrier::class);
     }
 }
