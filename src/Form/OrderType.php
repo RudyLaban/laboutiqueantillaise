e9022ecb2fb1a06241d13b17ccd97194a07dc074
Rudy <lay971@hotmail.fr>
2021-11-14 22:26:35 +0100

    Choix du transporteur + Récap paginer + màj style

--- src/Form/OrderType.php
+++ src/Form/OrderType.php
@@ -3,8 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Address;
+use App\Entity\Carrier;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,14 +17,28 @@ class OrderType extends AbstractType
         $user = $options['user'];
 
         $builder
-            ->add('adresses', EntityType::class, [
-                'label'     => 'Choisissez votre adresse',
+            ->add('addresses', EntityType::class, [
+                'label'     => 'Choisir mon adresse',
                 'required'  => true,
                 'class'     => Address::class,
                 'choices'   => $user->getAddresses(),
                 'multiple'  => false,
                 'expanded'  => true,
-            ]);
+            ])
+            ->add('carriers', EntityType::class, [
+                'label'     => 'Choisir mon transporteur',
+                'required'  => true,
+                'class'     => Carrier::class,
+                'multiple'  => false,
+                'expanded'  => true,
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Valider ma commande',
+                'attr'  => [
+                    'class' => 'btn btn-info btn-block'
+                ]
+            ])
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
